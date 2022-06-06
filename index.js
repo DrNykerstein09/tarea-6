90ce2effd03786e289fcca101ab0e3b1db6c5600
DrNykerstein09 <88530440+DrNykerstein09@users.noreply.github.com>
2022-06-05 19:41:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ listaDeCompra.push('Aceite de Girasol');
 listaDeCompra.pop();
 
 const peliculas = [
-    { titulo: 'Mobbie Dick', director: 'John Huston', fecha: 1981 },
-    { titulo: 'Pelicula2', director: 'Director2', fecha: 1982},
-    { titulo: 'Pelicula3', director: 'Director3', fecha: 1983 }
+    { titulo: 'Mobbie Dick', director: 'John Huston', fecha: new Date(2018, 9 , 24)  },
+    { titulo: 'Pelicula2', director: 'Director2', fecha: new Date(2015, 2 , 30) },
+    { titulo: 'Pelicula3', director: 'Director3', fecha: new Date(1989, 11, 26)  }
 ]
-const fil = peliculas.filter( titulo => titulo.fecha > 2000);
-console.log(fil)
+const fil = peliculas.filter( titulo => titulo.fecha < 2000, 1, 1 );
+console.log(fil);
+
+const directores = peliculas.map(dir => dir.director);
+console.log(directores);
+
+const titulos = peliculas.map(tit => tit.titulo);
+console.log(titulos);
+
+const concat = directores.concat(titulos);
+console.log(concat)
+
+const concat2 = [...directores, ...titulos];
+console.log(concat)
